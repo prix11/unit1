80b9571df4982db8061766dc97593209944517a9
prix11 <gesong49@gmail.com>
2022-03-14 15:37:42 +0200

    Main

---
+++ app/src/main/java/com/example/unit1/MainActivity.java
@@ -0,0 +1,12 @@
+package com.example.unit1;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+ @Override
+ protected void onCreate(Bundle savedInz) {
+
+ }
+}
--- app/src/main/java/com/example/unit1/index.java
+++
@@ -1,5 +0,0 @@
-package com.example.unit1;
-
-public class index {
-    const A = 127;
-}
